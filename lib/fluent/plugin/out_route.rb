96f66e852eae6ce08dd6a72f2ae71015dd25a2cb
Masahiro Nakagawa <repeatedly@gmail.com>
2016-11-11 17:21:26 +0900

    Add fluent/output require

--- lib/fluent/plugin/out_route.rb
+++ lib/fluent/plugin/out_route.rb
@@ -15,6 +15,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+require 'fluent/output'
 require 'fluent/mixin/config_placeholders'
 
 module Fluent
