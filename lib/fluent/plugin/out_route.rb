f09ad76a11952ada4e13c623d0af222802c4d65b
Masahiro Nakagawa <repeatedly@gmail.com>
2016-11-11 17:18:13 +0900

    Fix weird indent

--- lib/fluent/plugin/out_route.rb
+++ lib/fluent/plugin/out_route.rb
@@ -18,39 +18,86 @@
 require 'fluent/mixin/config_placeholders'
 
 module Fluent
+  class RouteOutput < MultiOutput
+    Plugin.register_output('route', self)
 
+    include Fluent::Mixin::ConfigPlaceholders
 
-class RouteOutput < MultiOutput
-  Plugin.register_output('route', self)
+    class Route
+      include Configurable
 
-  include Fluent::Mixin::ConfigPlaceholders
+      config_param :remove_tag_prefix, :string, :default => nil
+      config_param :add_tag_prefix, :string, :default => nil
+      # TODO tag_transform regexp
+      attr_accessor :copy
 
-  class Route
-    include Configurable
+      def initialize(pattern)
+        super()
+        if !pattern || pattern.empty?
+          pattern = '**'
+        end
+        @pattern = MatchPattern.create(pattern)
+        @tag_cache = {}
+      end
+
+      def match?(tag)
+        @pattern.match(tag)
+      end
+
+      def configure(conf)
+        super
+        if conf['copy']
+          @copy = true
+        end
+        if @remove_tag_prefix
+          @prefix_match = /^#{Regexp.escape(@remove_tag_prefix)}\.?/
+        else
+          @prefix_match = //
+        end
+        if @add_tag_prefix
+          @tag_prefix = "#{@add_tag_prefix}."
+        else
+          @tag_prefix = ""
+        end
+      end
+
+      def copy?
+        @copy
+      end
+
+      def emit(tag, es)
+        ntag = @tag_cache[tag]
+        unless ntag
+          ntag = tag.sub(@prefix_match, @tag_prefix)
+          if @tag_cache.size < 1024  # TODO size limit
+            @tag_cache[tag] = ntag
+          end
+        end
+        Engine.emit_stream(ntag, es)
+      end
+    end
+
+    def initialize
+      super
+      @routes = []
+      @tag_cache = {}
+      @match_cache = {}
+    end
 
     config_param :remove_tag_prefix, :string, :default => nil
     config_param :add_tag_prefix, :string, :default => nil
-    # TODO tag_transform regexp
-    attr_accessor :copy
+  # TODO tag_transform regexp
 
-    def initialize(pattern)
-      super()
-      if !pattern || pattern.empty?
-        pattern = '**'
-      end
-      @pattern = MatchPattern.create(pattern)
-      @tag_cache = {}
-    end
+    attr_reader :routes
 
-    def match?(tag)
-      @pattern.match(tag)
+    # Define `log` method for v0.10.42 or earlier
+    unless method_defined?(:log)
+      define_method("log") { $log }
     end
 
     def configure(conf)
       super
-      if conf['copy']
-        @copy = true
-      end
+
       if @remove_tag_prefix
         @prefix_match = /^#{Regexp.escape(@remove_tag_prefix)}\.?/
       else
@@ -61,103 +108,51 @@ class RouteOutput < MultiOutput
       else
         @tag_prefix = ""
       end
+
+      conf.elements.select {|e|
+        e.name == 'route'
+      }.each {|e|
+        route = Route.new(e.arg)
+        route.configure(e)
+        @routes << route
+      }
     end
 
-    def copy?
-      @copy
-    end
-
-    def emit(tag, es)
-      ntag = @tag_cache[tag]
-      unless ntag
+    def emit(tag, es, chain)
+      ntag, targets = @match_cache[tag]
+      unless targets
         ntag = tag.sub(@prefix_match, @tag_prefix)
-        if @tag_cache.size < 1024  # TODO size limit
-          @tag_cache[tag] = ntag
-        end
-      end
-      Engine.emit_stream(ntag, es)
-    end
-  end
-
-  def initialize
-    super
-    @routes = []
-    @tag_cache = {}
-    @match_cache = {}
-  end
-
-  config_param :remove_tag_prefix, :string, :default => nil
-  config_param :add_tag_prefix, :string, :default => nil
-  # TODO tag_transform regexp
-
-  attr_reader :routes
-
-  # Define `log` method for v0.10.42 or earlier
-  unless method_defined?(:log)
-    define_method("log") { $log }
-  end
-
-  def configure(conf)
-    super
-
-    if @remove_tag_prefix
-      @prefix_match = /^#{Regexp.escape(@remove_tag_prefix)}\.?/
-    else
-      @prefix_match = //
-    end
-    if @add_tag_prefix
-      @tag_prefix = "#{@add_tag_prefix}."
-    else
-      @tag_prefix = ""
-    end
-
-    conf.elements.select {|e|
-      e.name == 'route'
-    }.each {|e|
-      route = Route.new(e.arg)
-      route.configure(e)
-      @routes << route
-    }
-  end
-
-  def emit(tag, es, chain)
-    ntag, targets = @match_cache[tag]
-    unless targets
-      ntag = tag.sub(@prefix_match, @tag_prefix)
-      targets = []
-      @routes.each {|r|
-        if r.match?(ntag)
-          targets << r
-          break unless r.copy?
-        end
-      }
-      if @match_cache.size < 1024  # TODO size limit
-        @match_cache[tag] = [ntag, targets]
-      end
-    end
-
-    case targets.size
-    when 0
-      return
-    when 1
-      targets.first.emit(ntag, es)
-      chain.next
-    else
-      unless es.repeatable?
-        m = MultiEventStream.new
-        es.each {|time,record|
-          m.add(time, record)
+        targets = []
+        @routes.each {|r|
+          if r.match?(ntag)
+            targets << r
+            break unless r.copy?
+          end
         }
-        es = m
+        if @match_cache.size < 1024  # TODO size limit
+          @match_cache[tag] = [ntag, targets]
+        end
       end
-      targets.each {|t|
-        t.emit(ntag, es)
-      }
-      chain.next
+
+      case targets.size
+      when 0
+        return
+      when 1
+        targets.first.emit(ntag, es)
+        chain.next
+      else
+        unless es.repeatable?
+          m = MultiEventStream.new
+          es.each {|time,record|
+            m.add(time, record)
+          }
+          es = m
+        end
+        targets.each {|t|
+          t.emit(ntag, es)
+        }
+        chain.next
+      end
     end
   end
 end
-
-
-end
-
